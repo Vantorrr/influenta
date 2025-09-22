a80c5f572c7c1509e5724037eb2c966e67cba799
Pavel Galante <pavelgalante@gmail.com>
2025-09-22 13:08:17 +0300

    fix: add JwtModule to ChatModule to resolve WsJwtGuard dependency

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
@@ -7,7 +9,17 @@ import { Message } from './entities/message.entity';
 import { Response } from '../responses/entities/response.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message, Response])],
+  imports: [
+    TypeOrmModule.forFeature([Message, Response]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('app.jwt.secret'),
+        signOptions: { expiresIn: configService.get('app.jwt.expiresIn') },
+      }),
+    }),
+  ],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
   exports: [ChatService],
