3724bc1906bb22127a1d42116e8df3adbe962eec
Pavel Galante <pavelgalante@gmail.com>
2025-09-30 16:41:28 +0300

    fix(responses): import TelegramModule to resolve provider on prod

--- backend/src/responses/responses.module.ts
+++ backend/src/responses/responses.module.ts
@@ -6,9 +6,10 @@ import { ListingsModule } from '@/listings/listings.module';
 import { Blogger } from '@/bloggers/entities/blogger.entity';
 import { Advertiser } from '@/advertisers/entities/advertiser.entity';
 import { Listing } from '@/listings/entities/listing.entity';
+import { TelegramModule } from '@/telegram/telegram.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Response, Blogger, Advertiser, Listing]), ListingsModule],
+  imports: [TypeOrmModule.forFeature([Response, Blogger, Advertiser, Listing]), ListingsModule, TelegramModule],
   controllers: [ResponsesController],
   exports: [TypeOrmModule],
 })
