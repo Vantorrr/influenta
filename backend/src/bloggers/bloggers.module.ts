34d9e25fe147cd5f1840a12a22dcc6376975b09b
Pavel Galante <pavelgalante@gmail.com>
2025-11-05 12:44:14 +0300

    fix(bloggers): add SocialPlatform to module imports

--- backend/src/bloggers/bloggers.module.ts
+++ backend/src/bloggers/bloggers.module.ts
@@ -4,9 +4,10 @@ import { BloggersController } from './bloggers.controller';
 import { BloggersService } from './bloggers.service';
 import { User } from '@/users/entities/user.entity';
 import { Blogger } from './entities/blogger.entity';
+import { SocialPlatform } from '@/social-platforms/entities/social-platform.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Blogger])],
+  imports: [TypeOrmModule.forFeature([User, Blogger, SocialPlatform])],
   controllers: [BloggersController],
   providers: [BloggersService],
   exports: [BloggersService],
